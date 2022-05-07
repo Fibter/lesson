5c0f50f324c581d492841c8671d37af7822ae9aa
Your Name <your_email@whatever.com>
2022-05-07 13:33:50 +0300

    metodi

--- js/script.js
+++ js/script.js
@@ -1,128 +1,39 @@
 "use strict";
 
-//alert("hello");
-
-//const result = confirm ("are you heare"); 
-
-//const answer = prompt ("rewwffdf", "");   
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
- 
-
-for (i=0; i<2; i++) {   
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-          personalMovieDB.movies[a] = b;
-}
-
-console.log(personalMovieDB);   
-
-
-
-(num === 50) ? console.log('ok') : console.log('error');    //тернарный оператор  
-
-const number = 50;        //сравнение
-
-switch (number) { 
-        case 49:
-            console.log('error');
-            break;
-        case 45:
-            console.log('no');
-            break;
-         case 50:   
-            console.log('yes');
-            break;
-}
-
-
-const first = 40;      //сравнение по фложенности
-
-if (first < 30) { 
-    console.log('error');
-} else if (first >30) {   
-    console.log('good');
-}
-
-
-
-let yet =50;            //циклы
-
-while (yet < 55) {  
-    console.log(yet);
-    yet++;
+function test () {  
+    for (let i = 0; i < 5; i++ ) {   
+    console.log(i); 
+    if (i === 3) return }   
+    console.log("done");
 }   
 
-do {                     //сначало делать
-    console.log(yet);
-    yet++;
-}   
-while (yet < 55)    
+test () ;   
 
 
 
-for (let i=1; i<8; i++) {          //основа
-    console.log(yet);
-    yet++;
-}
+const str = "teSt";        //metodi
+
+console.log(str.toLowerCase());     
 
 
 
-let result = '';
-const length = 7;   
+const fruit = "SomeFruit";  
 
-for (let i = 1; i<length; i++) {    
-    for (let j=0; j<i; j++) {   
-        result += "*";
-    }   
-    result += '\n';
-}   
+console.log(fruit.indexOf());   
 
-console.log (result);
+const logg = "Hello World"; 
+
+console.log(logg.slice(6,9));   
+console.log(logg.substring(6,9));   
 
 
+const num = 12.2;   
+console.log(Math.round(num));   
 
-
-for (let i = 2; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
-
-
-let boy =10;
-
-function showFirstMessage (text) {  
-    console.log(text);  
-    boy= 10;
-}
-
-showFirstMessage("Hello World");    
-console.log(boy);   
-
-
-function calc (a,b) {   
-    return (a + b);
-}   
-
-console.log(calc(4,5));
+const rum = "12.2px";    
+console.log(parseInt(test));
 
 
 
-const logger = function () {    
-    console.log("hello");
-};  
-
-logger();   
 
 
-const calco = (a,b) => a+b;
